756dadc40a3ce69f7a116b91afbb7f2efc42faba
Tony Nguyen <tonngu137@gmail.com>
2025-11-26 23:23:52 +0100

    add layout system

--- wp-content/themes/block-theme/includes/gutenberg.php
+++ wp-content/themes/block-theme/includes/gutenberg.php
@@ -3,31 +3,31 @@
  * Remove Gutenberg Block Library CSS from loading on the frontend
  */
 
-function dequeue_wp_block_library_css(){
-    // wp_dequeue_style( 'wp-block-library' );
-   	// wp_dequeue_style( 'wp-emoji-styles' );
-		// wp_dequeue_style( 'global-styles' );
-} 
+// function dequeue_wp_block_library_css(){
+//     wp_dequeue_style( 'wp-block-library' );
+//    	wp_dequeue_style( 'wp-emoji-styles' );
+// 		wp_dequeue_style( 'global-styles' );
+// } 
 
-add_action( 'wp_enqueue_scripts', 'dequeue_wp_block_library_css', 100 );
+// add_action( 'wp_enqueue_scripts', 'dequeue_wp_block_library_css', 100 );
 
 
 
 /**
  *  Removes core blocks css
  */
-function prefix_remove_core_block_styles() {
-	global $wp_styles;
+// function prefix_remove_core_block_styles() {
+// 	global $wp_styles;
 
-	$keep = ["wp-block-navigation"];
-	foreach ( $wp_styles->queue as $key => $handle ) {
-		if ( (strpos( $handle, 'wp-block-' ) === 0  || strpos( $handle, 'wp-emoji-' ) === 0) && !in_array( $handle, $keep )   ) {
+// 	$keep = ["wp-block-navigation"];
+// 	foreach ( $wp_styles->queue as $key => $handle ) {
+// 		if ( (strpos( $handle, 'wp-block-' ) === 0  || strpos( $handle, 'wp-emoji-' ) === 0) && !in_array( $handle, $keep )   ) {
 
-			wp_dequeue_style( $handle );
-		}
-	}
-}
-add_action( 'wp_enqueue_scripts', 'prefix_remove_core_block_styles' );
+// 			wp_dequeue_style( $handle );
+// 		}
+// 	}
+// }
+// add_action( 'wp_enqueue_scripts', 'prefix_remove_core_block_styles' );
 
 
 
@@ -35,7 +35,7 @@ add_action( 'wp_enqueue_scripts', 'prefix_remove_core_block_styles' );
  *  Removes skip link css + js
  */
 
-remove_action( 'wp_footer', 'the_block_template_skip_link' );
+// remove_action( 'wp_footer', 'the_block_template_skip_link' );
 
 
 /**
@@ -43,4 +43,4 @@ remove_action( 'wp_footer', 'the_block_template_skip_link' );
  * @see https://wordpress.stackexchange.com/questions/413131/is-there-any-filter-or-action-hook-to-remove-layout-classes-from-appearing-in-my
  */
 
-add_theme_support( 'disable-layout-styles' );
+// add_theme_support( 'disable-layout-styles' );
